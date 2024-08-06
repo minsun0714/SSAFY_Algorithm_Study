2db781a4a735fbb1f908f9af6ffa65c9d77ed8bf
한재경 <hjg000223@gmail.com>
2024-08-06 17:22:11 +0900

    feat: N과M12 주석 수정

--- src/_8월2째주/백준_15666_N과M12_실버2_한재경_백트래킹.java
+++ src/_8월2째주/백준_15666_N과M12_실버2_한재경_백트래킹.java
@@ -23,11 +23,8 @@ public class 백준_15666_N과M12_실버2_한재경_백트래킹 {
         Set<Integer> set = new HashSet<>(Arrays.asList(origin));
         Integer[] nums = set.toArray(new Integer[0]);
 
-        // nums에서 m개 고른 오름차순 수열 (중복가능) 개수
         Arrays.sort(nums);
-        
-        // now에 현재까지의 수열 저장, idx에 인덱스(이상만 탐색) 저장해서 dfs 돌리기
-        // idx, idx+1 두가지 dfs 호출
+
         List<Integer> now = new ArrayList<>();
         dfs(nums, now, 0, m);
         System.out.println(result);
@@ -43,7 +40,7 @@ public class 백준_15666_N과M12_실버2_한재경_백트래킹 {
         }
         for (int i = idx; i < nums.length; i++) {
             now.add(nums[i]);
-            dfs(nums, now, i, m);
+            dfs(nums, now, i, m); //백트래킹
             now.remove(now.size() - 1);
         }
     }
