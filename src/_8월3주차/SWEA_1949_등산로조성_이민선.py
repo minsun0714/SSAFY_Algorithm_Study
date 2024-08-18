e4f3b9cbb8b7981734926376cf98c44ac04bf158
이민선 <woooooooow22@gmail.com>
2024-08-18 14:28:53 +0900

    visited 관리 방식 개선 (deep copy X)

--- src/_8월3주차/SWEA_1949_등산로조성_이민선.py
+++ src/_8월3주차/SWEA_1949_등산로조성_이민선.py
@@ -1,27 +1,28 @@
-import copy
-
+# 205ms
+# DFS
 T = int(input())
 
-def dfs(x, y, c, depth, visited):
+def dfs(x, y, c, depth):
     global answer
     answer = max(answer, depth)
 
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
 
-    copied_visited = copy.deepcopy(visited)
-    copied_visited[x][y] = 1
-
     for i in range(4):
         nx, ny = x + dx[i], y + dy[i]
 
         if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
             if board[nx][ny] < board[x][y]:  # 이동할 곳이 더 낮다면
-                dfs(nx, ny, c, depth + 1, copied_visited)
+                visited[nx][ny] = 1
+                dfs(nx, ny, c, depth + 1)
+                visited[nx][ny] = 0
             elif c == 1 and board[nx][ny] - k < board[x][y]:  # 깎아서 이동할 수 있다면
                 original = board[nx][ny]
                 board[nx][ny] = board[x][y] - 1  # 깎기
-                dfs(nx, ny, 0, depth + 1, copied_visited)  # 깎기 기회를 썼으므로 c를 0으로 설정
+                visited[nx][ny] = 1
+                dfs(nx, ny, 0, depth + 1)  # 깎기 기회를 썼으므로 c를 0으로 설정
+                visited[nx][ny] = 0
                 board[nx][ny] = original  # 원래 상태로 복구
 
 
@@ -37,6 +38,8 @@ for t in range(1, T + 1):
     for i in range(n):
         for j in range(n):
             if board[i][j] == max_val:
-                dfs(i, j, 1, 1, visited)  # 처음엔 깎을 기회가 1번 있음
+                visited[i][j] = 1
+                dfs(i, j, 1, 1)  # 처음엔 깎을 기회가 1번 있음
+                visited[i][j] = 0
 
     print(f"#{t} {answer}")
