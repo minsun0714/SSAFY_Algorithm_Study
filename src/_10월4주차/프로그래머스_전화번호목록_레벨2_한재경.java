56e367e29c9f4f1ef061d82ef1c21c25535ce2c0
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2024-10-28 17:37:47 +0900

    Update 프로그래머스_전화번호목록_레벨2_한재경.java

--- src/_10월4주차/프로그래머스_전화번호목록_레벨2_한재경.java
+++ src/_10월4주차/프로그래머스_전화번호목록_레벨2_한재경.java
@@ -5,13 +5,9 @@ class 프로그래머스_전화번호목록_레벨2_한재경 {
     public boolean solution(String[] phone_book) {
         
         Arrays.sort(phone_book);
-        for (int i = 0; i < phone_book.length; i++) {
-            for (int j = i + 1; j < phone_book.length; j++) { //i+1요소
-                if (phone_book[j].startsWith(phone_book[i])) {
-                    return false;
-                } else {
-                    break;
-                }
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                return false;
             }
         }
         return true;
