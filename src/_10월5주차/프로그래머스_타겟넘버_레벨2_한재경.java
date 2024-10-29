162f98260bda85048e5bcf617042ad83d5654d12
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2024-10-29 16:38:12 +0900

    Update 프로그래머스_타겟넘버_레벨2_한재경.java

--- src/_10월5주차/프로그래머스_타겟넘버_레벨2_한재경.java
+++ src/_10월5주차/프로그래머스_타겟넘버_레벨2_한재경.java
@@ -4,7 +4,6 @@ class 프로그래머스_타겟넘버_레벨2_한재경 {
     public void combinations(int[] numbers, int depth, int target, int n, int sum) {
         if (depth == n) {
             if (sum == target) {
-                System.out.println(sum);
                 cnt++;
             }
             return;
@@ -18,7 +17,6 @@ class 프로그래머스_타겟넘버_레벨2_한재경 {
     
     public int solution(int[] numbers, int target) {
         cnt = 0;
-        //n개 정수 +-해서 타겟넘버 만드는 경우의 수
         combinations(numbers, 0, target, numbers.length, 0);
         
         return cnt;
