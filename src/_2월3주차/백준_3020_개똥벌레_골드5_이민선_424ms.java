8aa4bfa515d966d1e482d4b9293af81d4958fa34
김강민 <kjj9500@naver.com>
2025-02-17 12:44:42 +0900

    백준_3020_개똥벌레_골드5_이민선_424ms.java

---
+++ src/_2월3주차/백준_3020_개똥벌레_골드5_이민선_424ms.java
@@ -0,0 +1,62 @@
+package _2월3주차;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 백준_3020_개똥벌레_골드5_이민선_424ms {
+  static int n, h;
+  static int[] bottom;
+  static int[] up;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    n = Integer.parseInt(st.nextToken());
+    h = Integer.parseInt(st.nextToken());
+
+    bottom = new int[n / 2];
+    up = new int[n / 2];
+
+    for (int i=0;i<n;i++){
+      int num = Integer.parseInt(br.readLine());
+      if (i % 2 == 0) bottom[i / 2] = num;
+      else up[i / 2] = num;
+
+    }
+    Arrays.sort(bottom);
+    Arrays.sort(up);
+    int minVal = 200_001;
+    int minValCount = 0;
+    for (int i=0;i<h;i++){
+      int target = h - i;
+      int countBottom = binarySearch(bottom, target);
+      target = i + 1;
+      int countUp = binarySearch(up, target);
+      if (minVal > n - countUp - countBottom){
+        minVal = n - countUp - countBottom;
+        minValCount = 1;
+      } else if (minVal == n - countUp - countBottom){
+        minValCount++;
+      }
+    }
+    System.out.println(minVal + " " + minValCount);
+  }
+
+  public static int binarySearch(int[] arr, int target){
+    int s = 0, e = n / 2 - 1;
+
+    while (s <= e){
+      int mid = (s + e) / 2;
+
+      if (arr[mid] < target){
+        s = mid + 1;
+      } else {
+        e = mid - 1;
+      }
+    }
+    return s;
+  }
+}
