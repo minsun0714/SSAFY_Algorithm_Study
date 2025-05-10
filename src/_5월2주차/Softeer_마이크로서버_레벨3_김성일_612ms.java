22ee54a536d200c08fa6d5394cd9156900adc1f1
Gitch-Hiker <131760773+Gitch-Hiker@users.noreply.github.com>
2025-05-10 20:44:52 +0900

    [ 612 ms | 56 mb ]

---
+++ src/_5월2주차/Softeer_마이크로서버_레벨3_김성일_612ms.java
@@ -0,0 +1,69 @@
+// Greedy
+// [ 612 ms | 56 mb ]
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer tokens;
+    static StringBuilder output = new StringBuilder();
+  
+    public static void main(String[] args) throws Exception{
+        int T = Integer.parseInt(input.readLine());
+        for(int t=0; t<T; t++){
+            TreeMap<Integer, Integer> map = new TreeMap<>();
+            int n = Integer.parseInt(input.readLine());
+            List<Integer> descSort = new ArrayList<>();
+            tokens = new StringTokenizer(input.readLine());
+            for(int i=0; i<n; i++){
+                descSort.add(Integer.parseInt(tokens.nextToken()));
+            }
+
+            descSort.sort(Comparator.reverseOrder());
+            for (int service : descSort) {
+                insertService(service, map);
+            }
+          
+            int count = 0;
+            for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+                count += entry.getValue();
+            }
+            output.append(count).append("\n");
+        } 
+
+        System.out.println(output);
+    }
+
+    private static void insertService(int service, TreeMap<Integer, Integer> map) {
+        int remain = 900-service;
+        if(map.isEmpty()){
+            insert(remain, map);
+            return;
+        }
+        int last = map.lastKey();
+        int totalRemain = last - service;
+        if(totalRemain>=0){ 
+            insert(totalRemain, map);
+            delete(last, map);
+        } else{ 
+            insert(remain,map);
+        }
+    }
+
+    private static void insert(int service, TreeMap<Integer, Integer> map) {
+        map.putIfAbsent(service, 0);
+        int count = map.get(service);
+        map.put(service, count+1);
+    }
+    private static void delete(int service, TreeMap<Integer, Integer> map) {
+        int count = map.get(service);
+        if(count==1){
+            map.remove(service);
+        } else{
+            map.put(service, count-1);
+        }
+    }
+
+}
